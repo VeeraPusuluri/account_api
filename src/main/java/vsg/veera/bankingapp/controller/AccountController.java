5f0027bf71980afed84d6c410cbd40f27dd53788
Veera <veera@Veeras-MacBook-Pro.local>
2024-03-29 20:23:11 +0530

    added script files

--- src/main/java/vsg/veera/bankingapp/controller/AccountController.java
+++ src/main/java/vsg/veera/bankingapp/controller/AccountController.java
@@ -1,16 +1,9 @@
 package vsg.veera.bankingapp.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import vsg.veera.bankingapp.data.AccountData;
-import vsg.veera.bankingapp.entity.Account;
-import vsg.veera.bankingapp.mapper.AccountMapper;
-import vsg.veera.bankingapp.repository.AccountRepository;
-import vsg.veera.bankingapp.service.AccountService;
 import vsg.veera.bankingapp.service.Imple.AccountServiceImpl;
 
 
